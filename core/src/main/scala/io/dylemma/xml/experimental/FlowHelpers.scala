94e1986361c03fc147ccc880d5ae402ebde5c1ab
Dylan Halperin <dylan@dylemma.io>
2016-02-09 23:10:29 -0500

    Convert some FlowHelpers to use GraphStage internally

--- core/src/main/scala/io/dylemma/xml/experimental/FlowHelpers.scala
+++ core/src/main/scala/io/dylemma/xml/experimental/FlowHelpers.scala
@@ -1,7 +1,8 @@
 package io.dylemma.xml.experimental
 
+import akka.stream.{Attributes, Outlet, Inlet, FlowShape}
 import akka.stream.scaladsl.Flow
-import akka.stream.stage.{SyncDirective, Context, PushPullStage}
+import akka.stream.stage._
 import io.dylemma.xml.Result
 import io.dylemma.xml.Result.{Empty, Error, Success}
 
@@ -12,18 +13,10 @@ import scala.collection.generic.CanBuildFrom
 	*/
 object FlowHelpers {
 
-	trait ParsingStage[A, B] extends PushPullStage[Result[A], Result[B]] {
-		protected def endResult: Result[B] = Empty
-
-		def onPull(ctx: Context[Result[B]]) = {
-			if(ctx.isFinishing) ctx.pushAndFinish(endResult)
-			else ctx.pull()
-		}
-		override def onUpstreamFinish(ctx: Context[Result[B]]) = ctx.absorbTermination()
-	}
-
-	def fromParsingStage[A, B](makeStage: () => ParsingStage[A, B]): Flow[Result[A], Result[B], akka.NotUsed] = {
-		Flow[Result[A]].transform(makeStage)
+	trait ConsumerGraphStage[A, B] extends GraphStage[FlowShape[Result[A], Result[B]]] {
+		val in: Inlet[Result[A]] = Inlet("ConsumerIn")
+		val out: Outlet[Result[B]] = Outlet("ConsumerOut")
+		override val shape = FlowShape(in, out)
 	}
 
 	trait TransformingStage[A, B] extends PushPullStage[Result[A], Result[B]] {
@@ -34,55 +27,88 @@ object FlowHelpers {
 		Flow[Result[A]].transform(makeStage)
 	}
 
-	def first[A] = fromParsingStage{() =>
-		new ParsingStage[A, A] {
-			def onPush(elem: Result[A], ctx: Context[Result[A]]) = {
-				if(elem.isEmpty) ctx.pull()
-				else ctx.pushAndFinish(elem)
+	trait ConsumerHandler[A, B] {
+		def onInput(input: Result[A]): Option[Result[B]]
+		def onEnd(): Result[B]
+	}
+
+	def fromConsumerHandler[A, B](makeHandler: () => ConsumerHandler[A, B]) = Flow.fromGraph{
+		new ConsumerGraphStage[A, B] {
+			def createLogic(inheritedAttributes: Attributes) = new GraphStageLogic(shape){
+				val handler = makeHandler()
+				setHandler(in, new InHandler {
+					def onPush() = {
+						val a = grab(in)
+						val bOpt = handler.onInput(a)
+						if(bOpt.isDefined){
+							push(out, bOpt.get)
+							completeStage()
+						} else {
+							pull(in)
+						}
+					}
+					override def onUpstreamFinish() = {
+						push(out, handler.onEnd())
+						completeStage()
+					}
+				})
+
+				setHandler(out, new OutHandler {
+					def onPull() = pull(in)
+				})
 			}
 		}
 	}
 
-	def firstOption[A] = fromParsingStage{() =>
-		new ParsingStage[A, Option[A]] {
-			override protected val endResult = Success.none
-
-			def onPush(elem: Result[A], ctx: Context[Result[Option[A]]]) = {
-				if(elem.isEmpty) ctx.pull()
-				else ctx.pushAndFinish(elem map (Some(_)))
+	def first[A] = fromConsumerHandler{ () =>
+		new ConsumerHandler[A, A] {
+			def onInput(input: Result[A]) = {
+				if(input.isEmpty) None
+				else Some(input)
 			}
+			def onEnd() = Empty
 		}
 	}
 
-	def toList[A] = fromParsingStage{() =>
-		new ParsingStage[A, List[A]] {
+	def firstOption[A] = fromConsumerHandler{ () =>
+		new ConsumerHandler[A, Option[A]] {
+			def onInput(input: Result[A]): Option[Result[Option[A]]] = {
+				if(input.isEmpty) None
+				else Some(input.map(Some(_)))
+			}
+			def onEnd() = Success.none
+		}
+	}
+
+	def toList[A] = fromConsumerHandler{ () =>
+		new ConsumerHandler[A, List[A]] {
 			private val builder = List.newBuilder[A]
-			override protected def endResult = Result(builder.result())
-			def onPush(elem: Result[A], ctx: Context[Result[List[A]]]) = elem match {
+			def onInput(input: Result[A]) = input match {
 				case Success(a) =>
 					builder += a
-					ctx.pull()
+					None
 				case Empty =>
-					ctx.pull()
+					None
 				case e: Error =>
-					ctx.pushAndFinish(e)
+					Some(e)
 			}
+			def onEnd() = Result(builder.result)
 		}
 	}
 
-	def concat[A, B, That](implicit t: A => TraversableOnce[B], bf: CanBuildFrom[A, B, That]) = fromParsingStage{() =>
-		new ParsingStage[A, That] {
+	def concat[A, B, That](implicit t: A => TraversableOnce[B], bf: CanBuildFrom[A, B, That]) = fromConsumerHandler{ () =>
+		new ConsumerHandler[A, That] {
 			private val builder = bf()
-			override protected def endResult = Result(builder.result())
-			def onPush(elem: Result[A], ctx: Context[Result[That]]) = elem match {
+			def onInput(input: Result[A]) = input match {
 				case Success(a) =>
 					builder ++= a
-					ctx.pull()
+					None
 				case Empty =>
-					ctx.pull()
+					None
 				case e: Error =>
-					ctx.pushAndFinish(e)
+					Some(e)
 			}
+			def onEnd() = Result(builder.result())
 		}
 	}
 
